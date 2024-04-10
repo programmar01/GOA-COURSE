d4ca29d0927ad3d577a4216b91eb83ec969148a7
programmar01 <matedolidze232@gmail.com>
2024-04-10 17:02:36 +0400

    second commit

---
+++ Day 51/script.js
@@ -0,0 +1,12 @@
+const myInfo = {
+    firstName: "mate",
+    lastName: "dolidze",
+    age: 15,
+}
+myInfo.firstName = "giorgi"
+
+
+console.log(myInfo.firstName);
+console.log(myInfo.lastName);
+console.log(myInfo.age);
+
---
+++ Day 52/script.js
@@ -0,0 +1,87 @@
+// function multiply(num1, num2, num3) {
+//     console.log(num1 * num2 * num3)
+// }
+// multiply(2, 3 ,2)
+
+// function info(firstName, lastName) {
+
+//     const fullInfo = firstName + ' ' + lastName
+//     return fullInfo
+// }
+// fullInfo("mate" , "dolidze")
+
+// function multy(firstNum, secondNUm){
+
+//     const multiply = firstNum*secondNUm
+//     return multiply
+// }
+
+
+// const multiplySecond = multiply(10 , 10) * 2
+// console.log(multiplySecond)
+
+
+
+
+
+
+
+
+
+// function add(firstNum, secondNUm){
+
+//     const ad = firstNum+secondNUm
+//     return ad
+// }
+
+// ad(17 , 10)
+// console.log(ad)
+// function minus(firstNum, secondNUm){
+
+//     const minu = firstNum-secondNUm
+//     return minu
+// }
+// minus(17 , 10)
+// console.log(minus)
+// function divison(firstNum, secondNUm){
+
+//     const divi = firstNum/secondNUm
+//     return divi
+// }
+
+
+// divi(10 , 2)
+// console.log(divi)
+// function multy(firstNum, secondNUm){
+
+//     const multiply = firstNum*secondNUm
+//     return multiply
+// }
+
+
+// multiply(10 , 10)
+// console.log(multiply)
+
+
+
+
+
+
+
+
+
+
+
+
+function rectangle(firstNum, secondNUm){
+
+    const perimeter = (firstNum + secondNUm) * 2
+    console.log(perimeter)
+
+    const fartobi = firstNum * secondNUm
+    consoole.log(fartobi)
+}
+
+
+perimeter(10 , 10)
+fartobi(10 , 10)
