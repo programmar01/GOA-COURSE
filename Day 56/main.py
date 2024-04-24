44e1090480a0121753f3066bf0aae99978a4ec44
programmar01 <matedolidze232@gmail.com>
2024-04-24 21:50:37 +0400

    second commit

---
+++ Day 56/main.py
@@ -0,0 +1,65 @@
+#first    
+
+
+
+
+
+
+
+
+# Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+
+# You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+
+
+#////////////////////////////
+
+# def litres(time):
+#     return int(time * 0.5)
+
+#////////////////////////////
+
+#/////////////////////////////////////////////////////////////////
+
+#second
+
+
+# Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+
+# Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+
+# Example:
+# n= 5, m=5: 25
+# n=-5, m=5:  0
+# Waiting for translations and Feedback! Thanks!
+
+
+#///////////////////////////
+
+# def paperwork(n, m):
+#     if n < 0:
+#         return (0)
+#     elif m < 0:
+#         return (0)
+#     else:
+#         return (n * m)
+    
+#////////////////////////////
+
+#//////////////////////////////////////////////////////////
+
+#third
+
+#////////////////////////////
+
+# def grow(arr):
+#     sum = 1
+#     for i in arr:
+#         sum = sum * i
+#     return sum
+
+#////////////////////////////
+
+#//////////////////////////////////////////////////////////
+
+#fourth
